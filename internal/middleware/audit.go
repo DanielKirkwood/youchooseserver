6ce6e1ae9101192fce711d4b9a48a0c2d0d6516a
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2023-04-16 21:56:13 +0100

    add audit middleware which adds request information to context

---
+++ internal/middleware/audit.go
@@ -0,0 +1,67 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type key string
+
+const (
+	AuditID key = "auditID"
+)
+
+type Action string
+
+// A Event stores information of a request.
+// Useful for logging and debugging requests.
+type Event struct {
+	ActorID    int       `db:"actor_id" json:"actor_id,omitempty"`
+	TableRowID int       `db:"table_row_id" json:"table_row_id,omitempty"`
+	Table      string    `db:"table_name" json:"table,omitempty"`
+	Action     Action    `db:"action" json:"action,omitempty"`
+	OldValues  string    `db:"old_values" json:"old_values,omitempty"`
+	NewValues  string    `db:"new_values" json:"new_values,omitempty"`
+	HTTPMethod string    `db:"http_method" json:"http_method,omitempty"`
+	URL        string    `db:"url" json:"url,omitempty"`
+	IPAddress  string    `db:"ip_address" json:"ip_address,omitempty"`
+	UserAgent  string    `db:"user_agent" json:"user_agent,omitempty"`
+	CreatedAt  time.Time `db:"created_at" json:"created_at"`
+}
+
+// Audit middleware adds an Event object to the context of
+// the request.
+func Audit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ev := Event{
+			ActorID:    getUserID(r),
+			HTTPMethod: r.Method,
+			URL:        r.RequestURI,
+			IPAddress:  readUserIP(r),
+			UserAgent:  r.UserAgent(),
+		}
+
+		ctx := context.WithValue(r.Context(), AuditID, ev)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// getUserID returns the value of userID in the request.
+func getUserID(r *http.Request) int {
+	val, _ := r.Context().Value("userID").(int)
+	return val
+}
+
+// readUserIP returns the given requests IP Addess.
+func readUserIP(r *http.Request) string {
+	ipAddress := r.Header.Get("X-Real-Ip")
+	if ipAddress == "" {
+		ipAddress = r.Header.Get("X-Forwarded-For")
+	}
+	if ipAddress == "" {
+		ipAddress = r.RemoteAddr
+	}
+	return ipAddress
+}
